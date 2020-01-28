4f143e83086f86d6b3d49820b15a4b3c1544bf7f
specnazm <laketic.milena98@gmail.com>
2020-01-28 16:14:21 +0100

    Added router, home and login page

---
+++ cloud/src/components/home/home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="home">
+    <h1>Cloud computing with AWS</h1>
+    <p>Amazon Web Services (AWS) is the world’s most comprehensive and broadly adopted cloud platform, offering over 175 fully featured services from data centers globally. Millions of customers—including the fastest-growing startups, largest enterprises, and leading government agencies—are using AWS to lower costs, become more agile, and innovate faster.</p>
+    <div class="cta">
+      <router-link to="/signin">Sign In</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #home {
+    width: 80%;
+    margin: auto;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  p {
+    text-align: center;
+  }
+
+  .cta {
+    width: 300px;
+    margin: auto;
+    text-align: center;
+  }
+
+  .cta a {
+    margin: 10px;
+    text-decoration: none;
+    display: inline-block;
+    border: 1px solid #521751;
+    border-radius: 3px;
+    width: 100px;
+    padding: 10px;
+    box-sizing: border-box;
+    color: #521751;
+  }
+
+  .cta a:hover,
+  .cta a:active {
+    background-color: #521751;
+    color: white;
+  }
+</style>
---
+++ cloud/src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import HomePage from './components/home/home.vue'
+import DashboardPage from './components/dashboard/dashboard.vue'
+import SigninPage from './components/auth/signin.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/signin', component: SigninPage },
+    { path: '/dashboard', component: DashboardPage }
+]
+
+export default new VueRouter({mode: 'history', routes})
