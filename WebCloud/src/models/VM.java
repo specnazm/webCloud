3a85630a3f4cdddeb1ffe5fe0e34c2a41140d4a5
Marko <marko.cvijanovic.1998@gmail.com>
2020-01-29 18:39:54 +0100

    Organisation endpoints.

--- WebCloud/src/models/VM.java
+++ WebCloud/src/models/VM.java
@@ -6,8 +6,15 @@ public class VM {
 	private String name;
 	private Organisation org;
 	private VMCategory category;
-	private int cpuCores;
-	private int gpuCores;
+	private Integer cpuCores;
+	private Integer gpuCores;
+	private Integer ram;
 	private HashMap<String, Disc> discs;
+	
+	public boolean checkRequired() {
+		if (this.name == null || this.cpuCores == null || this.gpuCores == null || this.ram == null || this.category == null)
+			return false;
+		return true;
+	}
 	
 }
--- WebCloud/src/rest/SparkMain.java
+++ WebCloud/src/rest/SparkMain.java
@@ -4,33 +4,37 @@ package src.rest;
 import static spark.Spark.port;
 import static spark.Spark.get;
 import static spark.Spark.post;
+import static spark.Spark.patch;
 import static spark.Spark.options;
 import static spark.Spark.before;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.google.gson.Gson;
 
 import spark.Session;
+import src.models.Organisation;
 import src.models.Roles;
 import src.models.User;
 
 public class SparkMain {
 		
 		private static Gson g = new Gson();
-		private static ArrayList<User> regUsers;
+		private static HashMap<String, User> regUsers;
+		private static HashMap<String, Organisation> regOrgs;
 		
 	public static void main(String[] args) {
 		port(8079);
 		
-		regUsers = new ArrayList<User>();
+		regOrgs = new HashMap<String, Organisation>();
+		regUsers = new HashMap<String, User>();
 		User super_admin = new User("superadmin@admin", "pass" , "superadmin", "superadmin" ,"" , Roles.SUPER_ADMIN);
-		regUsers.add(super_admin);
+		regUsers.put(super_admin.getEmail(), super_admin);
 		
 		
 		options("/*",
 		        (request, response) -> {
-		        	System.out.println("OVde");
 		            String accessControlRequestHeaders = request
 		                    .headers("Access-Control-Request-Headers");
 		            if (accessControlRequestHeaders != null) {
@@ -57,13 +61,10 @@ public class SparkMain {
 			User u = g.fromJson(payload, User.class);
 			Boolean authenticated = false;
 			User logged_user = new User();
-			for (User user : regUsers) {
-				if(user.verify(u.getEmail(), u.getPassword())) {
-					authenticated = true;
-					logged_user = user;
-				}
-					
-			}		
+			if (regUsers.containsKey(u.getEmail()) | regUsers.get(u.getEmail()).verify(u.getEmail(), u.getPassword())) {
+				authenticated = true;
+				logged_user = regUsers.get(u.getEmail());
+			}
 			if (authenticated) {
 				res.status(200);
 				ss.attribute("user", logged_user);
@@ -79,19 +80,132 @@ public class SparkMain {
 		get("/api/auth/logout" , (req, res) -> {
 			Session ss = req.session(true);
 			User u = ss.attribute("user");
-			String s = u.toString();
-			System.out.println(s);
 			
 			if(u != null) {
 				ss.invalidate();
 				res.status(200);
 			}else {
 				res.status(400);
-			}
-			
+				res.body("No user logged in.");
+				return res;
+			}			
 			
 			return res;
 		});
+		
+//ORGS	
+		get("/api/org" , (req, res) -> {
+			Session ss = req.session(true);
+			User u = ss.attribute("user");
+			res.type("application/json");
+			
+			if(u == null) {
+				res.status(400);
+				res.body("No user logged in, can't perform action.");
+				return res;
+			}
+			
+			if(u.getRole() != Roles.SUPER_ADMIN) {
+				res.status(403);
+				res.body("Action failed, user lacks permission.");
+				return res;
+			}else {
+				return g.toJson(regOrgs);
+			}
+			
+		});
+		
+		post("/api/org" , (req, res) -> {
+			Session ss = req.session(true);
+			User u = ss.attribute("user");
+			res.type("application/json");
+			
+			if(u == null) {
+				res.status(400);
+				res.body("No user logged in, can't perform action.");
+				return res;
+			}
+			
+			if(u.getRole() != Roles.SUPER_ADMIN) {
+				res.status(403);
+				res.body("Action failed, user lacks permission.");
+				return res;
+			}else {
+				String payload = req.body();
+				Organisation org = g.fromJson(payload, Organisation.class);
+				if(org.checkRequired()) {
+					if(regOrgs.containsKey(org.getName())) {
+						res.status(400);
+						res.body("Organisation with same name already exists.");
+						return res;
+					}else {
+						res.status(200);
+						regOrgs.put(org.getName(), org);
+						return g.toJson(org);
+					}					
+				}else {
+					res.status(400);
+					res.body("Fields missing.");
+					return res;
+				}
+			}
+			
+		});
+		
+		patch("/api/org/:org_id" , (req, res) -> {
+			Session ss = req.session(true);
+			User u = ss.attribute("user");
+			res.type("application/json");
+			String org_name = req.params("org_id");
+			
+			if(u == null) {
+				res.status(400);
+				res.body("No user logged in, can't perform action.");
+				return res;
+			}
+			
+			if(u.getRole() == Roles.USER) {
+				res.status(403);
+				res.body("Action failed, user lacks permission.");
+				return res;
+			}else {
+				String payload = req.body();
+				Organisation org = g.fromJson(payload, Organisation.class);
+				
+				if(org.getDesc()!= null) 
+				{
+					regOrgs.get(org_name).setDesc(org.getDesc());
+				}
+				
+				if(org.getLogo_url()!=null)
+				{
+					regOrgs.get(org_name).setLogo_url(org.getLogo_url());
+				}
+				
+				if(org.getName() != null)
+				{
+					if(org_name != org.getName())
+					{
+						if(regOrgs.containsKey(org.getName())) 
+						{
+							res.status(400);
+							res.body("Cannot change organisation name, such name already exists.");
+							return res;
+						}else 
+						{
+							regOrgs.get(org_name).setName(org.getName());
+							regOrgs.put(org.getName(), regOrgs.get(org_name));
+							regOrgs.remove(org_name);
+						}
+						
+					}
+				}
+				
+				return g.toJson(org);
+			}
+			
+		});
+		
 		
 		
 	}
